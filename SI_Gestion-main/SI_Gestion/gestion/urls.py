a92d87f8d0735caf22adf2bac2367bfc9de8c424
JordieRayanne <119185845+JordieRayanne@users.noreply.github.com>
2023-03-16 05:33:47 +0300

    ito

--- SI_Gestion-main/SI_Gestion/gestion/urls.py
+++ SI_Gestion-main/SI_Gestion/gestion/urls.py
@@ -6,5 +6,9 @@ urlpatterns = [
     path('choose', views.Choose, name='choose'),
     path('index', views.index, name='index'),
     path('form', views.Create_form , name='form'),
+    path('list', views.list_company , name='list'),
+    path('get_id_name/<int:idsociete>/<str:nom>', views.get_id_name, name='get_id_name'),    
+    path('log', views.log , name='log'),
+    path('insert_count', views.insert_count , name='insert_count'),
     path('add', views.insert_company, name='add'),
 ]
